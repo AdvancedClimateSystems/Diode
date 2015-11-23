65aaf15c780680a59164cc172488e8065fef577b
Auke Willem Oosterhoff <awoosterhoff@gmail.com>
2015-11-23 17:25:01 +0100

    Move error handling of Dispatcher.dispatch from decorator to method.

--- diode/dispatcher.py
+++ diode/dispatcher.py
@@ -23,8 +23,13 @@ def validate_json_rpc_request(data):
         raise InvalidRequestError
 
 
-def build_json_rpc_response(result, id):
-    """ Return a JSON-RPC formatted response object. """
+def build_json_rpc_response(result, id_):
+    """ Return a JSON-RPC formatted response object.
+
+    :param result: Result of a method call.
+    :param id_: Response id.
+    :return: JSON-RPC formatted string.
+    """
     return json.dumps({
         "jsonrpc": "2.0",
         "result": result,
@@ -32,7 +37,14 @@ def build_json_rpc_response(result, id):
     })
 
 
-def build_json_rpc_error(error, id_=None):
+def build_json_rpc_error(error, id_=False):
+    """ Create JSON-RPC error response. When `id_` is False, response id
+    is `None`.
+
+    :param error: Instance of JSON_RPCError.
+    :param id_: Response id, default is False.
+    :return: JSON-RPC formatted string.
+    """
     return json.dumps({
         "jsonrpc": "2.0",
         "error": error.to_json(),
@@ -69,7 +81,7 @@ class Dispatcher(object):
         """ Dispatch JSON-RPC request.
 
         :param msg: JSON-RPC formatted string.
-
+        :return: JSON-RPC formatted string.
         """
         try:
             try:
@@ -85,19 +97,26 @@ class Dispatcher(object):
                 raise InvalidParamsError
 
             try:
-                return build_json_rpc_response(result=result, id=data['id'])
+                return build_json_rpc_response(result=result, id_=data['id'])
             # Server must not reply when request doesn't contain 'id' attribute.
             except KeyError:
                 pass
-
         except JSON_RPCError as e:
-            return build_json_rpc_error(e)
+            return build_json_rpc_error(e, data.get('id', False))
         except:
-            return build_json_rpc_error(InternalError())
+            return build_json_rpc_error(InternalError(), data.get('id', False))
 
     @asyncio.coroutine
     def execute(self, jsonrpc, method, params=None, id=None):
-        """ Execute method mentioned in JSON-RPC request. """
+        """ Execute method mentioned in JSON-RPC request.
+
+        :param jsonrpc: Number specifying version of JSON-RPC protocol.
+        :param method: String containing name of method to invoke.
+        :param params: Method's arguments. Default None.
+        :param id: Request identifier.
+        :return: Result of method.
+        :raises: :class:`MethodNotFoundError`, when calling unknown method.
+        """
         try:
             fn = getattr(self, method)
         except AttributeError:
